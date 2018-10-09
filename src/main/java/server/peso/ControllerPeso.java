cd52ac7fbe5cf08a6e2b368761b04d84b67bd2d7
Gabriele Bolamperti 20016516 <20016516@studenti.uniupo.it>
2018-10-09 16:33:22 +0200

    Create funzioni di checkPeso e dayPesoOut. da testare complete

--- src/main/java/server/peso/ControllerPeso.java
+++ src/main/java/server/peso/ControllerPeso.java
@@ -3,26 +3,63 @@ package server.peso;
 import server.database.UserInitialDateDB;
 
 import java.io.ObjectStreamClass;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ControllerPeso {
 
-    private ArrayList<Float> list_min, list_max;
+    public static final DateTimeFormatter DTF = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    /*
+    LocalDate localDate = LocalDate.now();
+    HomestationSettings.DTF.format(localDate),
+    */
+
+
+    private static ArrayList<Float> list_min, list_max;
 
     public ControllerPeso(int homestation_id){
 
         Map<String, Object> map = UserInitialDateDB.getUserInitialDate(homestation_id);
 
+        LocalDate start_date = LocalDate.parse(String.valueOf(map.get("data_inizio_gravidanza")));
+        float start_peso = Float.valueOf(String.valueOf(map.get("peso")));
         boolean gemelli = Boolean.valueOf(String.valueOf(map.get("gemelli")));
         BMI bmi = getBMI(String.valueOf(map.get("BMI")));
-        System.out.println(bmi);
 
         list_min = getListSogliaMin(bmi, gemelli);
         list_max = getListSogliaMax(bmi, gemelli);
 
-        System.out.println(list_min);
-        System.out.println(list_max);
+        Map<String, Float> test_pesi = new HashMap<>();
+        test_pesi.put("2018-04-09", 63f);
+        test_pesi.put("2018-04-08", 62f);
+        test_pesi.put("2018-04-06", 61.5f);
+        test_pesi.put("2018-04-05", 61f);
+        test_pesi.put("2018-04-03", 60.5f);
+        test_pesi.put("2018-04-02", 60f);
+
+        System.out.println(DataFilter.dayPesoOut(start_date, start_peso, test_pesi));
+    }
+
+    public static int checkPeso(int week, float start_peso, float actual_peso){
+
+        if(actual_peso < (start_peso + list_min.get(week)))
+            return -1;
+
+        if(actual_peso > (start_peso + list_max.get(week)))
+            return 1;
+
+        return 0;
+    }
+
+    public static int getWeekOfPregnancy(LocalDate start_date, LocalDate date)
+    {
+        return (int) ChronoUnit.WEEKS.between(start_date, date);
     }
 
     public BMI getBMI(String bmi)
--- src/main/java/server/peso/DataFilter.java
+++ src/main/java/server/peso/DataFilter.java
@@ -1,5 +1,24 @@
 package server.peso;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Map;
+
 public class DataFilter {
 
+    public static int dayPesoOut(LocalDate start_date, float start_peso, Map<String, Float> pesi){
+        int count = 0;
+        Object[] date = pesi.keySet().toArray();
+        int last_date_index = date.length-1;
+        int week = ControllerPeso.getWeekOfPregnancy(start_date, LocalDate.parse(String.valueOf(date[last_date_index])));
+        while(ControllerPeso.checkPeso(week, start_peso, pesi.get(date[last_date_index]))!=0)
+        {
+            count++;
+            last_date_index--;
+            if(date[last_date_index]==null)
+                break;
+        }
+
+        return count;
+    }
 }
