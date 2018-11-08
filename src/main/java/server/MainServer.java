eb245ea70421ce504e6831485c6a178459593e95
Gabriele Bolamperti 20016516 <20016516@studenti.uniupo.it>
2018-11-08 21:13:11 +0100

    Eliminata apiLogin

--- src/main/java/server/MainServer.java
+++ src/main/java/server/MainServer.java
@@ -1,12 +1,9 @@
 package server;
 
-import server.api.v2.ApiLogin;
 import server.api.v2.ApiMedic;
 import server.api.v2.ApiPatient;
 import server.mqtt.MQTTSubscriber;
 import server.weight_control.WeightController;
-
-import java.time.LocalDate;
 
 public class MainServer {
 
@@ -21,8 +18,6 @@ public class MainServer {
         //new ApiService();
         new ApiPatient();
         new ApiMedic();
-
-        new ApiLogin();
 
         /*System.out.println("start");
         weightController.startcheck(1, LocalDate.parse("2018-01-02"), 0d);
